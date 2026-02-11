627f732ff232d5d08cb81df158003febba5ffd26
owen <pershing.owen.27@c-dh.org>
2026-02-11 13:40:56 -0600

    src stuff

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,22 +1,40 @@
 package model;
 
 public class Player { 
-    private double hungrinessIndex; // 0.0 to 1.0
+    private String name;
+    private int hungrinessIndex; // 0 to 5
     private double cashOnHand; //Money on hand
     private boolean cashOnly; //Only cash paying?
-    private boolean knowsAllOptions; //Have they checked all the different food trucks (Not relevant yet)
-    
     private double maxWillingnessToPay; //Most will pay
-    private double timePreference;      // True = prefers speed and False = prefers cheap
 
     public static void main(String[] args) {}
 
-    public Player(double hungrinessIndex, double cashOnHand, boolean cashOnly, double maxWillingnessToPay, double timePreference) {
+    public Player(String name, int hungrinessIndex, double cashOnHand, boolean cashOnly, double maxWillingnessToPay) {
+        this.name = name;
         this.hungrinessIndex = hungrinessIndex;
         this.cashOnHand = cashOnHand;
         this.cashOnly = cashOnly;
-        this.knowsAllOptions = false; // defaults to false as they start by knowing nothing
         this.maxWillingnessToPay = maxWillingnessToPay;
-        this.timePreference = timePreference;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getHungrinessIndex() {
+        return hungrinessIndex;
+    }
+
+    public double getCashOnHand() {
+        return cashOnHand;
+    }
+
+    public boolean isCashOnly() {
+        return cashOnly;
+    }
+
+    public double getMaxWillingnessToPay() {
+        return maxWillingnessToPay;
     }
 }
--- src/simulation/Demo.java
+++ src/simulation/Demo.java
@@ -2,28 +2,48 @@ package simulation;
 
 import truck.Truck;
 import model.MenuItem;
+import model.Player;
 
-import javax.imageio.plugins.tiff.ExifGPSTagSet;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import model.Ingredient;
 
 public class Demo {
     public static void main(String[] args) {
-        Truck owensTruck = new Truck("Owens food truck", 500);
-                                                                        //TYPE can be FRY, TOAST, BAKE, BOIL
-        Ingredient Bacon = new Ingredient("Bacon", true, 150, 10, 350, "FRY");
-        Ingredient Eggs  = new Ingredient("Eggs",  true,  80,  5, 200, "FRY");
-        Ingredient Toast = new Ingredient("Toast", false, 70,  3, 180, "TOAST");
+        Truck owensTruck = new Truck("Owens food truck", 500.0, 10, 20, 8, 6);
+        Truck billsTruck = new Truck("Bills truck", 195, 0, 3, 1, 3);
+        Player bill = new Player("Bill", 2, 27, true, 18);
+        Player luca = new Player("Luca", 1, 23, true, 8);
 
-        owensTruck.addIngredient(Eggs, 13);
-        owensTruck.addIngredient(Bacon, 12);
-        owensTruck.addIngredient(Toast, 20);
+        String owensJson = String.format("{\"name\":\"%s\",\"startingCash\":%.1f,\"xLoc\":%d,\"yLoc\":%d,\"length\":%d,\"width\":%d}",
+            owensTruck.getName(), owensTruck.getStartingCash(), owensTruck.getXLoc(), owensTruck.getYLoc(), owensTruck.getLength(), owensTruck.getWidth());
 
-        MenuItem fullBreakfast = new MenuItem("Full Breakfast", 12.00, Eggs, Eggs, Bacon, Toast);
+        String billsJson = String.format("{\"name\":\"%s\",\"startingCash\":%.1f,\"xLoc\":%d,\"yLoc\":%d,\"length\":%d,\"width\":%d}",
+            billsTruck.getName(), billsTruck.getStartingCash(), billsTruck.getXLoc(), billsTruck.getYLoc(), billsTruck.getLength(), billsTruck.getWidth());
 
-        owensTruck.cookAndPrepare(fullBreakfast.getIngredients());
+        String billJson = String.format("{\"name\":\"%s\",\"hungrinessIndex\":%d,\"cashOnHand\":%.2f,\"cashOnly\":%b,\"maxWillingnessToPay\":%.2f}",
+            bill.getName(), bill.getHungrinessIndex(), bill.getCashOnHand(), bill.isCashOnly(), bill.getMaxWillingnessToPay());
 
+        String lucaJson = String.format("{\"name\":\"%s\",\"hungrinessIndex\":%d,\"cashOnHand\":%.2f,\"cashOnly\":%b,\"maxWillingnessToPay\":%.2f}",
+            luca.getName(), luca.getHungrinessIndex(), luca.getCashOnHand(), luca.isCashOnly(), luca.getMaxWillingnessToPay());
 
-                
+        // Truck JSON array
+        String trucksJsonArray = String.format("[%s,%s]", owensJson, billsJson);
+
+        // Player JSON array  
+        String playersJsonArray = String.format("[%s,%s]", billJson, lucaJson);
+
+        try (FileWriter writer = new FileWriter("trucks.json")) {
+            writer.write(trucksJsonArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter writer = new FileWriter("players.json")) {
+            writer.write(playersJsonArray);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
